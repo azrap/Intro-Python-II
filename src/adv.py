59e73379864683e1368e50ce5ff6197415c27431
azrap <azraap@gmail.com>
2019-10-29 09:55:31 -0700

    MVP Day 1

--- src/adv.py
+++ src/adv.py
@@ -1,10 +1,10 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+    'outside':  Room("Outside Cave Entrance", "North of you, the cave mount beckons"),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
@@ -33,11 +33,23 @@ room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
-#
+# does this room have this attribute? If so, I'm going to get this attribute.
 # Main
 #
 
 # Make a new player object that is currently in the 'outside' room.
+
+player1 = Player("Azra", room['outside'])
+
+
+def room_swapper(room, direction):
+    new_room = getattr(room, f"{direction}_to")
+    if new_room is None:
+        print('There are no rooms in that direction, please try again\n')
+        new_room = room
+    else:
+        print('room switch was successful!\n')
+    return new_room
 
 # Write a loop that:
 #
@@ -49,3 +61,26 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+# create a helper version to do the movement
+# has attr and get attr, google it.
+
+
+q = False
+while q is False:
+    print(
+        f"Greetings {player1.name}, your current room is: {player1.room.name}\n")
+    direction = input(
+        "which direction will you choose? (n,s,e,w) or enter q to quit: \n")
+    direction = direction.lower().strip()[0]
+    if direction in ["n", "s", "e", "w"]:
+        player1.room = room_swapper(player1.room, direction)
+    elif direction == "q":
+        print("thank you for playing, good bye")
+        q = True
+    else:
+        print("Invalid input. Type n,s,e,w to pick direction or q to quit:\n")
+
+    # if direction.upper()=="N":
+    #     player1.current_room=[key for (key, value) in room.items() if value==room[f'{player1.current_room}'].n_to ]
+    # # if direction.upper()=="S":
+    #     player1.current_room=room[f'{player1.current_room}'].s_to #change it to dynamic later
